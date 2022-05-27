351d786f821e51df5d27d995446d080e40ad3889
27b <baldassani@tuta.com>
2022-05-26 22:29:03 -0300

    Adding models

--- point_of_sale/models.py
+++ point_of_sale/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class Category(models.Model):
+    models.CharField(max_length=32, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+class Item(models.Model):
+    category_id = models.OneToOneField(Category, on_delete=models.SET_NULL)
+    visibility = models.BooleanField(default=False)
+    title = models.CharField(min_length="3", max_length=64)
+    image = models.ImageField(upload_to='images/item/')
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+    last_modified = models.DateField(auto_now=True)
+    datetime = models.DateTimeField(auto_now_add=True)
+
+
+class Transaction(models.Model):
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    items = ArrayField(models.JSONField(default={}))
+    datetime = models.DateTimeField(auto_now_add=True)
