c1f68751ef0780c0c22cb51d845e5226e2f570c0
27b <baldassani@tuta.io>
2022-05-27 22:56:46 -0300

    Fixed models of point_of_sale

--- point_of_sale/models.py
+++ point_of_sale/models.py
@@ -3,24 +3,33 @@ from django.contrib.postgres.fields import ArrayField
 
 
 class Category(models.Model):
-    models.CharField(max_length=32, unique=True)
+    title = models.CharField(max_length=32, default="", unique=True)
 
     class Meta:
         verbose_name_plural = 'Categories'
 
+    def __str__(self):
+        return self.title
+
 
 class Item(models.Model):
-    category_id = models.OneToOneField(Category, on_delete=models.SET_NULL)
+    category_id = models.OneToOneField(Category, on_delete=models.DO_NOTHING)
     visibility = models.BooleanField(default=False)
-    title = models.CharField(min_length="3", max_length=64)
-    image = models.ImageField(upload_to='images/item/')
+    title = models.CharField(max_length=64)
+    image = models.ImageField(upload_to='images/item/', null=True, blank=True)
     unit_price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.IntegerField()
     last_modified = models.DateField(auto_now=True)
     datetime = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Transaction(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    items = ArrayField(models.JSONField(default={}))
+    items = ArrayField(models.JSONField())
     datetime = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id + ": " + self.datetime)
