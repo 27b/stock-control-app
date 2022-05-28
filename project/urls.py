4f2ad1f05de3db12e5d374a417817bbc27ad7b32
27b <baldassani@tuta.io>
2022-05-27 23:11:30 -0300

    Fixed project urls

--- project/urls.py
+++ project/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/admin-panel/', include('point_of_sale.urls')),
-    path('api/point-of-sale/', include('admin_panel.urls')),
+    path('api/admin-panel/', include('admin_panel.urls')),
+    path('api/point-of-sale/', include('point_of_sale.urls')),
     path('', include('frontend.urls'))
 ]
