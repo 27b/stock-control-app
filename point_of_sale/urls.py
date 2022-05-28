c7cf3ab022456d17e6112254ffb5fa46d92855e6
27b <baldassani@tuta.io>
2022-05-27 23:10:19 -0300

    Adding views and setting models in admin

--- point_of_sale/urls.py
+++ point_of_sale/urls.py
@@ -1,4 +1,13 @@
 from django.urls import path
+from rest_framework import routers
 from . import views
 
-urlpatterns = []
+router = routers.SimpleRouter()
+router.register(r"item", views.ItemView, basename="item")
+router.register(r"transaction", views.TransactionView, basename="transaction")
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name="Index")
+]
+
+urlpatterns += router.urls
--- point_of_sale/views.py
+++ point_of_sale/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.views.generic import View
+from django.http import HttpResponse, HttpRequest
+from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from .models import Item, Transaction
+from .serializers import ItemSerializer, TransactionSerializer
 
-# Create your views here.
+
+class IndexView(View):
+
+    def get(self, request: HttpRequest) -> HttpResponse:
+        return HttpResponse(f"Hello World!\n{self}")
+
+
+class ItemView(viewsets.ModelViewSet):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    http_method_names = ["get"]
+
+
+class TransactionView(viewsets.ModelViewSet):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+    http_method_names = ["get"]
