be73bd8d20849962a3a39622e09ceaa99b5e95f9
27b <baldassani@tuta.io>
2022-05-29 23:52:32 -0300

    Adding category view

--- point_of_sale/urls.py
+++ point_of_sale/urls.py
@@ -3,6 +3,7 @@ from rest_framework import routers
 from . import views
 
 router = routers.SimpleRouter()
+router.register(r"category", views.CategoryView, basename="category")
 router.register(r"item", views.ItemView, basename="item")
 router.register(r"transaction", views.TransactionView, basename="transaction")
 
--- point_of_sale/views.py
+++ point_of_sale/views.py
@@ -2,9 +2,9 @@ from django.views.generic import View
 from django.http import HttpResponse, HttpRequest
 from rest_framework import viewsets
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
-from .models import Item, Transaction
-from .serializers import ItemSerializer, TransactionSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .models import Category, Item, Transaction
+from .serializers import CategorySerializer, ItemSerializer, TransactionSerializer
 
 
 class IndexView(View):
@@ -13,12 +13,20 @@ class IndexView(View):
         return HttpResponse(f"Hello World!\n{self}")
 
 
-class ItemView(viewsets.ModelViewSet):
+class CategoryView(viewsets.ModelViewSet):
     authentication_classes = [SessionAuthentication, BasicAuthentication]
     permission_classes = [IsAuthenticated]
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    http_method_names = ["get", "post"]
+
+
+class ItemView(viewsets.ModelViewSet):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAdminUser]
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
-    http_method_names = ["get"]
+    http_method_names = ["get", "post"]
 
 
 class TransactionView(viewsets.ModelViewSet):
