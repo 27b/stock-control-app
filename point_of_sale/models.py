6709e5f40d941bf638272afcadfe92dfea2b0e67
27b <baldassani@tuta.io>
2022-05-30 23:56:55 -0300

    Adding .check_transaction_integrity in Transaction model

--- point_of_sale/models.py
+++ point_of_sale/models.py
@@ -28,8 +28,38 @@ class Item(models.Model):
 
 class Transaction(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    items = ArrayField(models.JSONField())
+    items = ArrayField(models.JSONField())  # {"item_id", "quantity", "unit_price"}
     datetime = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.id + ": " + self.datetime)
+
+    def check_transaction_integrity(self) -> list[dict]:
+        """
+        Checks if all items in a transaction are valid.
+        """
+        messages = []
+        for item in self.items:
+            item_in_db = Item.query.filter(Item.id == item.id).first()
+
+            # Check if the item exists and is available to the user.
+            if not item_in_db or not item_in_db.visibility:
+                text = f"{item.title} not available."
+                messages += {"error": text}
+
+            # Check if the quantity of products is available.
+            if item.quantity < 0:
+                text = f"The quantity of {item.title} must be positive."
+                messages += {"error": text}
+
+            if item.quantity > item_in_db.quantity:
+                db_quantity = item_in_db.quantity
+                text = f"{item.title} only has {db_quantity} units in stock."
+                messages += {"error": text}
+
+            # Check if the price is correct.
+            if item.unit_price != item_in_db.unit_price:
+                text = f"{item.title} has the wrong price."
+                messages += {"error": text}
+
+        return messages
