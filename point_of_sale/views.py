d613ab8d4bf2f0b2222d58b023dfea89b13d1e0f
27b <baldassani@tuta.io>
2022-05-30 23:59:44 -0300

    Updating transaction view

--- point_of_sale/views.py
+++ point_of_sale/views.py
@@ -1,37 +1,57 @@
 from django.views.generic import View
-from django.http import HttpResponse, HttpRequest
-from rest_framework import viewsets
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from django.http import HttpResponse
+from rest_framework import viewsets, status
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.response import Response
+
 from .models import Category, Item, Transaction
+from .permissions import PointOfSalePermission
 from .serializers import CategorySerializer, ItemSerializer, TransactionSerializer
 
 
 class IndexView(View):
 
-    def get(self, request: HttpRequest) -> HttpResponse:
-        return HttpResponse(f"Hello World!\n{self}")
+    @staticmethod
+    def get():
+        return HttpResponse("Hello, World!")
 
 
 class CategoryView(viewsets.ModelViewSet):
-    authentication_classes = [SessionAuthentication, BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [PointOfSalePermission]
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    http_method_names = ["get", "post"]
+    http_method_names = ["get"]
 
 
 class ItemView(viewsets.ModelViewSet):
-    authentication_classes = [SessionAuthentication, BasicAuthentication]
-    permission_classes = [IsAdminUser]
-    queryset = Item.objects.all()
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [PointOfSalePermission]
+    queryset = Item.objects.filter(visibility=True).all()
     serializer_class = ItemSerializer
-    http_method_names = ["get", "post"]
+    http_method_names = ["get"]
 
 
 class TransactionView(viewsets.ModelViewSet):
-    authentication_classes = [SessionAuthentication, BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [PointOfSalePermission]
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
-    http_method_names = ["get"]
+    http_method_names = ["post"]
+
+    def create(self, request):
+        transaction = self.serializer_class(data=request.data)
+        if transaction.is_valid():
+            transaction_integrity = transaction.check_transaction_integrity()
+            if transaction_integrity == []:
+                Transaction.objects.create_user(**transaction.validated_data)
+                return Response(transaction, status=status.HTTP_201_CREATED)
+            return Response({
+                'errors': transaction_integrity,
+                'data': transaction.validated_data
+            }, status=status.HTTP_404_NOT_FOUND)
+        return Response({
+            'status': 'Bad Request',
+            'message': 'Account could not be created with received data'
+        }, status=status.HTTP_400_BAD_REQUEST)
+
