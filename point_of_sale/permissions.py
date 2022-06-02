b48e9b675be2b9f90a20fd749332aa7226bdd24b
27b <baldassani@tuta.io>
2022-06-02 18:34:58 -0300

    Fixed permission by role

--- point_of_sale/permissions.py
+++ point_of_sale/permissions.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework.permissions import BasePermission
 
 
 class PointOfSalePermission(BasePermission):
@@ -6,6 +6,6 @@ class PointOfSalePermission(BasePermission):
     Check if the user have the rol of point_of_sale.
     """
     def has_permission(self, request, view) -> bool:
-        if bool(request.user) and request.user.profile.role == "point_of_sale":
+        if bool(request.user) and request.user.role == "PS":
             return True
         return False
