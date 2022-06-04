662872aa5537f40e3928dddbadb850f8301b9292
27b <baldassani@tuta.io>
2022-06-04 17:36:20 -0300

    Adding React and project structure

---
+++ frontend/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './Assets/app.css';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <h1>Inventory Management</h1>
+  </React.StrictMode>
+);
+
+reportWebVitals();
