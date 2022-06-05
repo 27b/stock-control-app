c2d651d0f3992c251acc8bfd3ba2906afc0a3ec0
27b <baldassani@tuta.io>
2022-06-05 20:00:52 -0300

    Fixed error frontend.urls

--- project/urls.py
+++ project/urls.py
@@ -5,6 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/admin-panel/', include('admin_panel.urls')),
-    path('api/point-of-sale/', include('point_of_sale.urls')),
-    path('', include('frontend.urls'))
+    path('api/point-of-sale/', include('point_of_sale.urls'))
 ]
