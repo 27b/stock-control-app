556b3aa5199010094c7a83d4d6107ef19f8d509f
27b <baldassani@tuta.io>
2022-06-12 21:49:24 -0300

    Minor changes in frontned files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './assets/app.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- frontend/src/routes/DashboardRoutes.js
+++ frontend/src/routes/DashboardRoutes.js
@@ -2,7 +2,6 @@ import React, { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { PATH } from "../constants/paths"
 
-const AdminIndex = lazy(() => import("../pages/Admin/index"));
 const PointOfSaleIndex = lazy(() => import("../pages/PointOfSale/index"));
 
 export default function DashboardRoutes() {
