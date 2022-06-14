e2c12e2256e1f64dd0924047eae871bf6ee1333e
27b <baldassani@tuta.io>
2022-06-14 19:35:20 -0300

    Updating AuthAPI

--- frontend/src/services/Auth/AuthAPI.js
+++ frontend/src/services/Auth/AuthAPI.js
@@ -1,1 +1,32 @@
-export const saveUser = token => localStorage.setItem('token', token);
+const USER_DATA_URL = 'http://localhost:8000/api/point-of-sale/user/';
+
+export const loginUser = token => localStorage.setItem('token', token);
+
+export const logoutUser = () => localStorage.removeItem('token');
+
+export const checkUser = async () => sendAuthRequest('GET', USER_DATA_URL).json();
+
+export const authUser = async () => localStorage.getItem('token') && checkUser;
+
+export const authAdminUser = async () => localStorage.getItem('token') && checkUser.data.role === 'AD';
+
+export const sendRequest = async (method, url, data=null) => {
+    return await fetch (url, {
+        method: method,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    })
+}
+
+export const sendAuthRequest = async (method, url, data=null) => {
+    return await fetch (url, {
+        method: method,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Token ' + localStorage.getItem('token'),
+        },
+        body: JSON.stringify(data)
+    })
+}
