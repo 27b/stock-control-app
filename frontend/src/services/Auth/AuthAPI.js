ab1ebc1e05f3826f47d0178d435bbd3e03643485
27b <baldassani@tuta.io>
2022-06-15 17:11:32 -0300

    Updating AuthAPI

--- frontend/src/services/Auth/AuthAPI.js
+++ frontend/src/services/Auth/AuthAPI.js
@@ -1,32 +1,43 @@
-const USER_DATA_URL = 'http://localhost:8000/api/point-of-sale/user/';
+export const USER_DATA_URL = 'http://localhost:8000/api/point-of-sale/user/';
 
-export const loginUser = token => localStorage.setItem('token', token);
+export const loginUser = ({token, user}) => {
+    localStorage.setItem('token', token);
+    localStorage.setItem('id', user.id);
+    localStorage.setItem('username', user.username);
+    localStorage.setItem('role', user.role);
+};
 
-export const logoutUser = () => localStorage.removeItem('token');
+export const logoutUser = () => localStorage.clear();
 
-export const checkUser = async () => sendAuthRequest('GET', USER_DATA_URL).json();
+export const checkToken = () => localStorage.getItem('token') && localStorage.getItem('token').length === 40;
 
-export const authUser = async () => localStorage.getItem('token') && checkUser;
+export const checkUser = () => localStorage.getItem('username') && localStorage.getItem('role') && localStorage.getItem('id');
 
-export const authAdminUser = async () => localStorage.getItem('token') && checkUser.data.role === 'AD';
+export const authUser = () => checkToken() && checkUser();
+
+export const authAdminUser = () => authUser() && localStorage.getItem('role') === 'AD';
 
 export const sendRequest = async (method, url, data=null) => {
-    return await fetch (url, {
+    let options = {
         method: method,
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify(data)
-    })
+    }
+    if (method !== 'get' && method !== 'GET')
+        options['body'] = JSON.stringify(data);
+    return await fetch (url, options);
 }
 
 export const sendAuthRequest = async (method, url, data=null) => {
-    return await fetch (url, {
+    let options = {
         method: method,
         headers: {
             'Content-Type': 'application/json',
             'Authorization': 'Token ' + localStorage.getItem('token'),
         },
-        body: JSON.stringify(data)
-    })
+    }
+    if (method !== 'get' && method !== 'GET')
+        options['body'] = JSON.stringify(data);
+    return await fetch(url, options);
 }
