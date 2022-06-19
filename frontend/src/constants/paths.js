4ff9ed072f5a84b1a0b9227d2b8861ea4161eef7
27b <baldassani@tuta.io>
2022-06-18 23:23:05 -0300

    Adding admin and static files

--- frontend/src/constants/paths.js
+++ frontend/src/constants/paths.js
@@ -1,6 +1,7 @@
 export const PATH = {
     LOGIN: '/',
     DASHBOARD: '/dashboard',
+    ADMIN: '/admin',
     ITEM: '/item',
     CATEGORY: '/category',
     TRANSACTION: '/transaction',
--- frontend/src/routes/DashboardRoutes.js
+++ frontend/src/routes/DashboardRoutes.js
@@ -2,6 +2,8 @@ import React, { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { PATH } from "../constants/paths"
 import UserGuard from "../guards/UserGuard";
+import AdminGuard from "../guards/AdminGuard";
+import AdminIndex from '../pages/Admin';
 
 const PointOfSaleIndex = lazy(() => import("../pages/PointOfSale/index"));
 
@@ -10,6 +12,9 @@ export default function DashboardRoutes() {
       <Routes>
         <Route element={<UserGuard />}>
          <Route exact="true" path={PATH.DASHBOARD} element={<PointOfSaleIndex />} />
+        </Route>
+        <Route element={<AdminGuard />}>
+         <Route exact="true" path={PATH.ADMIN} element={<AdminIndex />} />
         </Route>
       </Routes>
     )
