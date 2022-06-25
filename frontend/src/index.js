91122b88c7f4a27b54f717e131cb83cda23d5034
27b <baldassani@tuta.io>
2022-06-25 17:52:58 -0300

    Rename folder app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import App from './App/App';
+import App from './app/App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
