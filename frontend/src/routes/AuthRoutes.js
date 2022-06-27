fa1b23e3190472efd2f6307fe5b8c90df18df0c1
27b <baldassani@tuta.io>
2022-06-26 23:11:00 -0300

    Fixed login import

--- frontend/src/routes/AuthRoutes.js
+++ frontend/src/routes/AuthRoutes.js
@@ -2,7 +2,7 @@ import React, { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { PATH } from "../constants/paths"
 
-const Login = lazy(() => import("../pages/Auth/Login"));
+const Login = lazy(() => import("../pages/Auth/login"));
 
 export default function AuthRoutes() {
     return (
