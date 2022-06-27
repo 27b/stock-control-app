935f97b95b7e12e4c3b367483502155a48de3189
27b <baldassani@tuta.io>
2022-06-26 23:10:06 -0300

    Update Admin categories page

--- frontend/src/pages/Admin/categories.js
+++ frontend/src/pages/Admin/categories.js
@@ -1,33 +1,30 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import ReactDOM from 'react-dom';
 import { useNavigate } from "react-router-dom";
 import { CategoryHandler } from '../../services/AdminAPI';
 import Layout from './_layout';
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 import Spinner from 'react-bootstrap/Spinner';
-import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form'
 
-const STATE = [];
+const STATE = {categories: []};
 
 const CategoryItem = ({id, title}) => {
+    const navigate = useNavigate();
     const remove = false;
     
-    const Update = useCallback(() => {
-        const div = document.createElement('div');
-        div.id = 'modal';
-        document.getElementById('root').appendChild(div);
-        ReactDOM.render(<ModalForm id={id} title={title} />, document.getElementById('modal'))
-    });
+    const Update = () => {
+        navigate('./' + id);
+    }
 
     const Delete = useCallback( async () => {
-        await CategoryHandler().delete(id);
-    });
+        await CategoryHandler.delete(id);
+    }, []);
 
     useEffect(() => {
         if (remove) {
-            for (const category in STATE) if (category.id == id) STATE.push(category);
+            for (const category in STATE.categories)
+                if (category.id == id)
+                    STATE.categories.push(category);
         }
     });
 
@@ -49,27 +46,25 @@ const CategoryList = ({categories}) => {
         const listOfCategories = categories.map(category => <CategoryItem key={category.id} id={category.id} title={category.title} />);
         return listOfCategories;
     }
-    else {
-        return (
-            <td>
-                <Spinner animation="grow" role="status">
-                    <span className="visually-hidden">Loading...</span>
-                </Spinner>
-            </td>
-        )
-    }
 }
 
 const AdminCategories = () => {
     const [state, setState] = useState(STATE);
+ 
     const get_categories = async () => {
-        const data = await CategoryHandler().get();
-        const result = await data.json();
-        setState(result);
+        try {
+            const data = await CategoryHandler.get();
+            const result = await data.json();
+            setState(result.results);
+        }
+        catch (error) {
+            console.log('Category error: ' + error);
+        }
     }
 
     useEffect(() => {
         get_categories();
+        STATE.categories = state
     })
 
     return (
@@ -86,53 +81,14 @@ const AdminCategories = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        <CategoryList categories={state} />
+                        <CategoryList categories={STATE.categories} />
                     </tbody>
                 </Table>
-                <div id='modal'></div>
             </>
         } />
     )
 }
 
-const ModalForm = ({id, title}) => {
-    const initialState = {id: '', title: ''};
-    const [state, setState] = useState(initialState);
-    const [show, setShow] = useState(true);
-  
-    const handleClose = () => {
-        document.getElementById('modal').remove();
-        setShow(false);
-    }
-
-    const handleInputChange = e => setState({...state, [e.target.name]: e.target.value});
-
-    const handleSubmit = event => {
-        event.preventDefault();
-    }
-
-    useEffect(() => setState({id, title}), []);
-  
-    return (
-        <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-            <Modal.Title>Category { id }</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form onSubmit={handleSubmit}>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Control name="title" placeholder="Category Title"
-                        onChange={handleInputChange} value={state.title} />
-                    </Form.Group>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={handleClose}>Close</Button>{' '}
-                        <Button variant="primary" type="submit">Save Changes</Button>
-                    </Modal.Footer>
-                </Form>
-            </Modal.Body>
-        </Modal>
-    );
-}
 
 
 export default AdminCategories;
