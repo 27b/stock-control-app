4704b6702fe940c8fc93960555329be348b532c6
27b <baldassani@tuta.io>
2022-06-28 14:04:13 -0300

    Updating Routes

--- frontend/src/pages/Admin/index.js
+++ frontend/src/pages/Admin/index.js
@@ -1,7 +1,9 @@
 import _AdminDashboard from './dashboard';
-import _AdminCategories from './categories';
-import _AdminItems from './items';
+import _AdminCategories, { AdminCategoryItem as _AdminCategoryItem } from './categories';
+import _AdminItems, { AdminItemDetail as _AdminItemDetail } from './items';
 
 export const AdminDashboard = _AdminDashboard;
 export const AdminCategories = _AdminCategories;
+export const AdminCategoryItem = _AdminCategoryItem;
 export const AdminItems = _AdminItems;
+export const AdminItemDetail = _AdminItemDetail;
--- frontend/src/routes/AdminRoutes.js
+++ frontend/src/routes/AdminRoutes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { PATH } from "../constants/paths"
 import AdminGuard from "../guards/AdminGuard";
-import { AdminCategories, AdminDashboard, AdminItems } from '../pages/Admin';
+import { AdminCategories, AdminCategoryItem, AdminDashboard, AdminItems, AdminItemDetail } from '../pages/Admin';
 
 export default function AuthRoutes() {
     return (
@@ -10,7 +10,10 @@ export default function AuthRoutes() {
         <Route element={<AdminGuard />}>
          <Route path={PATH.ADMIN.INDEX} element={<AdminDashboard />} />
          <Route path={PATH.ADMIN.CATEGORY} element={<AdminCategories />} />
+         <Route path={PATH.ADMIN.CATEGORY + '/:id'} element={<AdminCategoryItem />} />
          <Route path={PATH.ADMIN.ITEM} element={<AdminItems />} />
+         <Route path={PATH.ADMIN.ITEM + '/:id'} element={<AdminItemDetail />} />
+
         </Route>
       </Routes>
     )
