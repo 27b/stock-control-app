53d40d749915f50fe5a1f87a7dd46f00448e07af
27b <baldassani@tuta.io>
2022-07-01 21:14:26 -0300

    Adding frontend Header component

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+const HeaderLayout = ({left, right}) => {
+    return (
+        <div className='d-flex justify-content-between align-items-center mb-3'>
+            { left }
+            { right }
+        </div>
+    )
+}
+
+export const AdminPanelHeader = ({title, link, button}) => {
+    return (
+        <HeaderLayout
+            left={ <h1>{ title }</h1> }
+            right={ <Link className="text-white bg-primary p-2 rounded" to={ link }>{ button }</Link> }
+        />
+    )
+}
+
+export default HeaderLayout;
