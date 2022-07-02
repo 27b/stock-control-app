d700dc1d106c9cc2cbe2a2f1a9d76853f22a25df
27b <baldassani@tuta.io>
2022-07-01 21:16:03 -0300

    Updating frontend routes

--- frontend/src/routes/AdminRoutes.js
+++ frontend/src/routes/AdminRoutes.js
@@ -2,7 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { PATH } from "../constants/paths"
 import AdminGuard from "../guards/AdminGuard";
-import { DashboardView, CategoryListView, CategoryDetailView, ItemListView, ItemDetailView } from '../pages/Admin';
+import { 
+  DashboardView,
+  CategoryListView,
+  CategoryDetailView,
+  ItemListView,
+  ItemAddView,
+  ItemDetailView
+} from '../pages/Admin';
 
 export default function AuthRoutes() {
     return (
@@ -12,6 +19,7 @@ export default function AuthRoutes() {
          <Route path={PATH.ADMIN.CATEGORY} element={<CategoryListView />} />
          <Route path={PATH.ADMIN.CATEGORY + '/:id'} element={<CategoryDetailView />} />
          <Route path={PATH.ADMIN.ITEM} element={<ItemListView />} />
+         <Route path={PATH.ADMIN.ITEM + '/add'} element={<ItemAddView />} />
          <Route path={PATH.ADMIN.ITEM + '/:id'} element={<ItemDetailView />} />
         </Route>
       </Routes>
--- frontend/src/routes/UserRoutes.js
+++ frontend/src/routes/UserRoutes.js
@@ -3,13 +3,13 @@ import { Route, Routes } from 'react-router-dom';
 import { PATH } from "../constants/paths"
 import UserGuard from "../guards/UserGuard";
 
-const PointOfSaleIndex = lazy(() => import("../pages/PointOfSale/index"));
+const PointOfSaleView = lazy(() => import("../pages/PointOfSale/dashboard"));
 
 export default function DashboardRoutes() {
     return (
       <Routes>
         <Route element={<UserGuard />}>
-         <Route exact="true" path={PATH.USER.INDEX} element={<PointOfSaleIndex />} />
+         <Route exact="true" path={PATH.USER.INDEX} element={<PointOfSaleView />} />
         </Route>
       </Routes>
     )
