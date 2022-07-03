a6f380ef6eb95d5e0a879d479b46300e5484eb6c
27b <baldassani@tuta.io>
2022-07-03 20:44:41 -0300

    Updating frontend Navbar component

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { Navbar } from "react-bootstrap";
+import Dropdown from 'react-bootstrap/Dropdown'
 
 const NavbarComponent = () => {
-    const username = localStorage.getItem('username');
+    const username = localStorage.getItem('username') + ' ';
 
     return (
-        <Navbar className="bg-white mb-4 container">
-            <Navbar.Brand href="#home">Inventory Management</Navbar.Brand>
-            <Navbar.Toggle />
-            <Navbar.Collapse className="justify-content-end">
-                <Navbar.Text>
-                    Signed in as: <a href="#login">{ username }</a>
-                </Navbar.Text>
-            </Navbar.Collapse>
+        <Navbar className="bg-white mb-4 border-bottom">
+            <div className="container">
+                <Navbar.Brand>Inventory Management</Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse className="justify-content-end">
+                    <Navbar.Text>
+                    <Dropdown>
+                        <Dropdown.Toggle className="text-capitalize" variant="outline-secondary" id="dropdown-basic">
+                            { username }
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu align={{ lg: 'end' }}>
+                            <Dropdown.Item href="/logout">Logout</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    </Navbar.Text>
+                </Navbar.Collapse>
+            </div>
         </Navbar>
     )
 }
