eae5c80ea9423e72debe4656db263537dda93340
27b <baldassani@tuta.io>
2022-07-04 22:30:20 -0300

    Adding frontend User service

--- frontend/src/services/AdminAPI.js
+++ frontend/src/services/AdminAPI.js
@@ -2,11 +2,14 @@ import { HTTPHandler, sendAuthRequest } from './http/index';
 import { SERVER_URL } from '../constants/urls';
 
 const DASHBOARD_URL = SERVER_URL + 'api/point-of-sale/dashboard/';
+const USER_URL = SERVER_URL + 'api/point-of-sale/user/';
 const CATEGORY_URL = SERVER_URL + 'api/point-of-sale/category/';
 const ITEM_URL = SERVER_URL + 'api/point-of-sale/item/';
 const TRANSACTION_URL = SERVER_URL + 'api/point-of-sale/transaction/';
 
 export const DashboardHandler = new HTTPHandler(DASHBOARD_URL, sendAuthRequest);
+
+export const UserHandler = new HTTPHandler(USER_URL, sendAuthRequest);
 
 export const CategoryHandler = new HTTPHandler(CATEGORY_URL, sendAuthRequest);
 
