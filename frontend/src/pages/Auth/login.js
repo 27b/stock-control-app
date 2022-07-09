a2010029d4eca1082688c547c9bb41639ef9fd2e
27b <baldassani@tuta.io>
2022-07-08 21:49:33 -0300

    Updating frontend login form

--- frontend/src/pages/Auth/login.js
+++ frontend/src/pages/Auth/login.js
@@ -26,32 +26,31 @@ const Login = () => {
     }
 
     return (
-        <div class="div-center text-center">
-            <main class="form-signin w-100 m-auto align-center">
+        <div className="div-center text-center">
+            <main className="form-signin w-100 m-auto align-center">
                 <form onSubmit={handleSubmit}>
-                    <h1 class="h3 mb-3 fw-normal">Sign in</h1>
-                    <h3 class="h6 mb-3 fw-normal">Sign in to continue</h3>
-
-                    <div class="form-floating">
+                    <h1 className="h3 mb-3 fw-normal">Sign in</h1>
+                    <h3 className="h6 mb-3 fw-normal">Sign in to continue</h3>
+                    <div className="form-floating">
                         <input
-                            type="username" class="form-control" id="floatingInput" placeholder="name@example.com"
+                            type="username" className="form-control" id="floatingInput" placeholder="name@example.com"
                             name="username" value={state.username} onChange={handleInputChange}
                         />
-                        <label for="floatingInput">Email address</label>
+                        <label>Username</label>
                     </div>
-                    <div class="form-floating">
+                    <div className="form-floating">
                         <input
-                            type="password" class="form-control" id="floatingPassword" placeholder="Password"
+                            type="password" className="form-control" id="floatingPassword" placeholder="Password"
                             name="password" value={state.password} onChange={handleInputChange}
                         />
-                        <label for="floatingPassword">Password</label>
+                        <label>Password</label>
                     </div>
 
-                    <div class="checkbox mb-3">
+                    <div className="checkbox mb-3">
                         <label> <input type="checkbox" value="remember-me" /> Remember me </label>
                     </div>
-                    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-                    <p class="mt-5 mb-3 text-muted">
+                    <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+                    <p className="mt-5 mb-3 text-muted">
                         <a href="/">Forgot password?</a>
                     </p>
                 </form>
