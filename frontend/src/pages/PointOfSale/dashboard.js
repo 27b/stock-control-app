3d923f90f38fdec2e543a80e09ad2dee629d5911
27b <baldassani@tuta.io>
2022-07-14 22:55:44 -0300

    Fix frontend dashboard

--- frontend/src/pages/PointOfSale/dashboard.js
+++ frontend/src/pages/PointOfSale/dashboard.js
@@ -4,24 +4,20 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { ItemHandler, TransactionHandler } from '../../services/AdminAPI';
 
-const ItemListState = [];
-
-const ItemModal = ({data}) => {
-    const add = () => { ItemListState.push(data) }
-
+const ItemModal = ({data, add}) => {
     return (
         <div className="p-1 clearfix">
-            <span>{ data.title }</span>
+            <span>{ data.title } ({ data.quantity })</span>
             <Button
                 className="float-end" variant="primary" as="input" type="button" value="Add"
-                onClick={add}
+                onClick={() => add(data)}
             />
         </div>
     )
 }
 
-const ItemModalList = () => {
-    const [state, setState] = useState([]);
+const ItemModalList = ({func}) => {
+    const [itemList, setState] = useState([]);
     
     const getData = () => {
         ItemHandler.get()
@@ -34,44 +30,37 @@ const ItemModalList = () => {
     return (
         <>
             {
-                state.map(data => <ItemModal key={data.id} data={data} />)
+                itemList.map(data => <ItemModal key={data.id} data={data} add={func} />)
             }
         </>
     )
 }
 
-const ItemAdded = ({data}) => {
+const ItemAdded = ({data, remove}) => {
     return (
         <div className="pt-2 pb-2 w-100 d-flex justify-content-between align-items-center">
-            {data.title}
-            <Button variant="danger" as="input" type="button" value="X" />
+            { data.title }
+            <Button variant="danger" as="input" type="button" value="X" onClick={() => remove(data)} />
         </div>
     )
 }
 
-const ItemAddedList = () => {
-    const [state, setState] = useState([]);
-
-    useEffect(() => { setState(ItemListState) }, [])
-
-    return (
-        <>
-            {
-                state.map(data => <ItemAdded data={data} />)
-            }
-        </>
-    )
-}
-
 const PointOfSaleView = () => {
+    const [state, setState] = useState([]);
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-    const sendItems = () => {
-        const data = ItemListState;
-        TransactionHandler.post(data);
+    const addItemToFatherState = newItem => {
+        const itemsEqualToNewItem = state.filter(item => item.id === newItem.id);
+        if (itemsEqualToNewItem.length === 0) {
+            state.push(newItem);
+        }
     }
+
+    const removeItemToFatherState = data => { setState(state.filter(item => item.id !== data.id)) }
+
+    const postTransaction = () => { console.log('Hello World!') }
 
     return (
         <>
@@ -80,24 +69,26 @@ const PointOfSaleView = () => {
                 <div className="w-25 p-3 border rounded bg-light">
                     <div className="pb-2">
                         <h3>Added items</h3>
-                        <ItemAddedList />
+                        {
+                            state.map(data => <ItemAdded key={data.id} data={data} remove={removeItemToFatherState} />)
+                        }
                     </div>
                     <p className="border-top pt-3 text-secondary d-flex justify-content-between">
                         <div>Price:</div>
                         <div>127.20</div>
                     </p>
                     <div className="d-flex justify-content-between border-top pt-3">
-                        <Button variant="success" as="input" type="button" value="Success" onClick={sendItems} />{' '}
+                        <Button variant="success" as="input" type="button" value="Success" onClick={postTransaction} />{' '}
                         <Button variant="primary" as="input" type="button" value="Add Items" onClick={handleShow} />
                     </div>
                 </div>
             </div>
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
-                <Modal.Title>Add Item</Modal.Title>
+                    <Modal.Title>Add Item</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <ItemModalList />
+                    <ItemModalList func={ addItemToFatherState } />
                 </Modal.Body>
             </Modal>
         </>
